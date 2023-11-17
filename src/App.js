fb68db5c2ca3827e43ab20a828693f5b22550a61
andrey <examle@gmail.com>
2023-11-18 02:58:40 +0300

    pizza1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import './index.scss'
+import './scss/app.scss'
+import Header from './components/Header'
+import Sort from './components/Sort';
+import Categories from './components/Categories';
+import PizzaBlock from './components/PizzaBlock'
+import pizzas from './assets/pizzas.json'
 
+console.log(pizzas);
 
 function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {
+              pizzas.map(item => <PizzaBlock key={item.title} {...item} />)
+            }
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 
 }
 
